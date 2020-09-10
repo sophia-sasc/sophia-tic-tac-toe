ab9d271e4877a9914baf1a525d6896aac8b34d6e
sophia-sasc <sophia-sasc@dataindependence.net>
2020-09-09 19:30:11 -0600

    1 file modified

--- index.ts
+++ index.ts
@@ -3,8 +3,13 @@ var tdElements = document.getElementsByTagName("TD");
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
-
+var 
 function handleClick(event) {
   var target = event.target;
+  if (isO) { 
+    target.innerHTML = "O";
+isO
+  }
+
   target.innerHTML = "X";
 }
