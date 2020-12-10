53fbe03eb107bda25def30d2a3932e3856d3b7e8
sophia-sasc <sophia-sasc@dataindependence.net>
2020-12-09 19:25:51 -0700

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ var tdElements = document.getElementsByTagName("TD");
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
-var 
+var isO = true
 function handleClick(event) {
   var target = event.target;
   if (isO) { 
@@ -12,4 +12,14 @@ isO
   }
 
   target.innerHTML = "X";
+}
+var board = [
+  //
+  ["X", null, null], //
+  ["X", "O", null], //
+  ["X", "O", null] //
+];
+
+if (board[2][2]) {
+  // alert(board[1][0]);
 }
