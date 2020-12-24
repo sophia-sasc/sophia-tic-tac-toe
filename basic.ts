4c48450c799ce929636bf52e59099793fba61f52
russoturisto <russoturisto@gmail.com>
2020-12-23 19:19:37 -0700

    Dec 23rd 2020 update

--- basic.ts
+++ basic.ts
@@ -1,16 +1,20 @@
 export var basicVar = "basic";
+
 var tdElements = document.getElementsByTagName("TD");
-var board =  
-[[null, null , null], 
-[null, null , null], 
-[null, null, null]];
-if (board[2][2]) {
-  alert(board[2][2]);
-}
+
+var board = [
+  //
+  [null, null, null], // rowIndex="0"
+  [null, null, null], // rowIndex="1"
+  [null, null, null] // rowIndex="2"
+];
+
 for (var tdElement of tdElements) {
   tdElement.addEventListener("click", handleClick);
 }
+
 var isTheClickForAnO = false;
+
 function handleClick(event) {
   var targetTd = event.target;
   if (isTheClickForAnO) {
